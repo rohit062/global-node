794e1dd33bae1c667891dc6931789706342b050b
Rohit Salaria <rohit.salaria@jungleworks.com>
2019-01-01 20:28:22 +0530

    file read feature added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # global-js
 this module helps to manage function context(this) and application global variables.
+
--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const global = require('./lib/global');
-global.setProperty('key',{a:1,b:2});
-global.setProperty('key1',{a:1,b:2});
 
-global.setProperty('key2',{a:1,b:2});
-
-global.setProperty('key3',{a:1,b:2});
-
-console.log(global.deleteProperty('key11'));
-
-
+module.exports = global;
